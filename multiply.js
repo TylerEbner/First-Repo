2e044601decd1c43517a3298a6f5b5f6110b361a
TylerEbner <30875263+TylerEbner@users.noreply.github.com>
2017-08-16 09:24:16 -1000

    I made a calculator.

---
+++ multiply.js
@@ -0,0 +1,14 @@
+//Ask user for a number
+//Ask user for another number
+//Take both numbers and multiply them
+//Output the value to the screen using a window popup
+
+var firstQuestion = prompt("Please choose a number");
+var anotherQuestion = promt("Please choose another number");
+var thirdNumber = (firstQuestion*anotherQuestion);
+window.alert(thirdNumber);
+
+
+
+
+
