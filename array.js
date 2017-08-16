75d83ce2defec4693257829ac45d79a87babf64b
TylerEbner <30875263+TylerEbner@users.noreply.github.com>
2017-08-16 08:44:29 -1000

    I added favFoods.length

---
+++ array.js
@@ -0,0 +1,6 @@
+var favFoods = ["Bacon", "Lettuce", "Tomato"];
+//there are 3 strings//
+var students = ["Michael", "Raene", "Chuck", "Ryan"];
+//there are 4 strings//
+window.alert(students[2] + " " + favFoods[0]);
+//this will pop up as "Chuck Bacon"
